7d1d4c2477d9c13d48f8b4deefe1528981376930
Maxim Loginov <maximloginov22@gmail.com>
2024-10-21 16:29:04 +0500

    Lection4

--- TodoServerApp/Data/ApplicationDbContext.cs
+++ TodoServerApp/Data/ApplicationDbContext.cs
@@ -5,5 +5,19 @@ namespace TodoServerApp.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public virtual DbSet<TaskItem> TaskItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<TaskItem>().HasData([
+                new TaskItem { Id = 1, Title = "Задача 1", Description = "Описание 1", CreatedDate = DateTime.Now},
+                new TaskItem { Id = 2, Title = "Задача 2", Description = "Описание 2", CreatedDate = DateTime.Now},
+                new TaskItem { Id = 3, Title = "Задача 3", Description = "Описание 3", CreatedDate = DateTime.Now},
+                new TaskItem { Id = 4, Title = "Задача 4", Description = "Описание 4", CreatedDate = DateTime.Now},
+                new TaskItem { Id = 5, Title = "Задача 5", Description = "Описание 5", CreatedDate = DateTime.Now},
+                new TaskItem { Id = 6, Title = "Задача 6", Description = "Описание 6", CreatedDate = DateTime.Now},
+                ]);
+        }
     }
 }
---
+++ TodoServerApp/Data/Migrations/20241021111720_add_tasks.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TodoServerApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_tasks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TaskItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    FinishDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskItems", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TaskItems",
+                columns: new[] { "Id", "CreatedDate", "Description", "FinishDate", "Title" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 10, 21, 16, 17, 19, 938, DateTimeKind.Local).AddTicks(8157), "Описание 1", null, "Задача 1" },
+                    { 2, new DateTime(2024, 10, 21, 16, 17, 19, 938, DateTimeKind.Local).AddTicks(8167), "Описание 2", null, "Задача 2" },
+                    { 3, new DateTime(2024, 10, 21, 16, 17, 19, 938, DateTimeKind.Local).AddTicks(8168), "Описание 3", null, "Задача 3" },
+                    { 4, new DateTime(2024, 10, 21, 16, 17, 19, 938, DateTimeKind.Local).AddTicks(8169), "Описание 4", null, "Задача 4" },
+                    { 5, new DateTime(2024, 10, 21, 16, 17, 19, 938, DateTimeKind.Local).AddTicks(8170), "Описание 5", null, "Задача 5" },
+                    { 6, new DateTime(2024, 10, 21, 16, 17, 19, 938, DateTimeKind.Local).AddTicks(8171), "Описание 6", null, "Задача 6" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TaskItems");
+        }
+    }
+}
